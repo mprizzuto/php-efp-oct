adef768377c95af5a165be5a1772135c3509e3e6
Marco R <mprizzuto88@gmail.com>
2021-10-18 22:38:45 -0400

    create regexs for password checking

--- password-checker/index.php
+++ password-checker/index.php
@@ -1,26 +1,77 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>PHP password checker</title>
+		<title>PHP anagram checker</title>
 	</head>
 	<body>
+	<?php
+	
 
-	<?php 
+	// echo preg_match_all($pattern, $sentence, $matches);
+
 	function formatVar($input) {
 		echo "<pre>";
 		var_dump($input);
 		echo "</pre>";
 	}
+	// formatVar($_POST);
+	$password = $_POST["the-password"] ?? null;
+
+	function passwordValidator($password) {
+		$userMessage = "";
+		// $lettersAndNums = "/^[a-zA-Z]\d{1}+$/";
+		$lettersAndNums = "/^[a-zA-Z \d{1}]+$/";
+		// $sentence = "a0";
+		$numsOnly = "/^\d+$/";
+		$lettersOnly = "/^[a-zA-Z]+$/";
+		$lettersNumsSpecialChars = "/./";
+
+		if ( trim($password) === "" ) {
+			return "required!" . 0;
+		} 
+
+		elseif ( strlen($password) < 8 and preg_match_all($numsOnly, $password, $matches) === 1) {
+			return "very weak password, you only entered less than 8 chars and only nums" . 1;
+		}
+
+		elseif ( strlen($password) < 8 and preg_match_all($lettersOnly, $password, $matches) === 1) {
+			return "weak password, less than 8 chars long and only letters" . 3;
+		}
+
+		elseif ( strlen($password) >= 8 and preg_match_all($lettersAndNums, $password, $matches) === 1)  {
+			return "strong password, you entered atleast 8 letters an atleast one number" . 6;
+		} 
+
+		elseif ( strlen($password) >= 8 and preg_match_all($lettersNumsSpecialChars, $password, $matches) ) {
+			return "very strong password, you entered atleast 8 alphanumeric characters and atleast one special char" . 10;
+		}
+	}
+
+	// echo passwordValidator("hfhrjsh5#");
 
 	?>
 
+	<h1>type a password</h1>
+
+	<p>and we will tell you how strong it is</p>
+
+	<p>a number from 1-10 is returned. 1 is a very weak password and 10 is a very strong password</p>
+
 	<form method="post" action="<?=htmlspecialchars($_SERVER["PHP_SELF"])?>">
-		<label>
-			<input type="" name="">
+		<label for="the-password">type a password
+			<input type="password" name="the-password" id="the-password">
 		</label>
 
 		<input type="submit" value="submit">
 	</form>
+	<?php if ($_SERVER["REQUEST_METHOD"] === "POST"): ?>
+		<?=passwordValidator($password)?>
+	<?php endif?>
+
+
+	<script>
+		console.clear();
+	</script>
 	</body>
 </html>
--- password-checker/pseudocode.md
+++ password-checker/pseudocode.md
@@ -1,1 +1,9 @@
-pseudocode.md
+# password checker
+
+- check if string is a very weak PW: is only numbers and less than 8 chars long
+
+- check if PW is weak. fewer than 8 chars and only letters
+
+- check if PW is  strong. contains letters and atleast one num. atleast 8 chars long
+
+- cehck if PW is  very strong. contains atleast 8 chars and letters, nums, and special chars
